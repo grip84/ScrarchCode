9a01c69f95a3034486c49550da71ca7911ec44ab
Robert Curley <robertcurley@5cg5231dg8.state.in.us>
2018-09-03 14:26:52 -0400

    new

---
+++ payload_general.js
@@ -0,0 +1,33 @@
+// Setting the machine variable with properties from payload.
+var machine = payload.get("machine");
+
+// Below I will be using the readModel method to return the vCACEntity.  To better understand I am going to give each a parameter a meaningful variable.
+var vmProps = new Properties(); //Initializing a new Object
+
+vmProps.put("VirtualMachineID", machine.get("id"));  // Setting machineID properties
+hostId = vCACHost.id;  //This the vCACHost in our case the web\model server iotvmmp81fw
+modelName = "ManagementModelEntities.svc";  //vCAC IaaS extensibility meta-model
+entitySetName = "VirtualMachines";  //This is an entity set within the model.  VirtualMachines are the the VMs deployed from blueprints
+virtualMachineEntity = vCACEntityManager.readModelEntity(hostId, modelName, entitySetName, vmProps, null);  //Put it all together and create the virtualMachineEntity to create the vmProperties array
+vmProperties = new Properties();  //Create vmProperties 
+
+//Creating an array of all of the vm properties the virtualMachineEntity object 
+var virtualMachinePropertiesEntities = virtualMachineEntity.getLink(vCACHost, "VirtualMachineProperties");
+
+//Looping through entity properties to create vmProperties array with found properties
+for each (var virtualMachinePropertiesEntity in virtualMachinePropertiesEntities) {
+    var propertyName = virtualMachinePropertiesEntity.getProperty("PropertyName");
+    var propertyValue = virtualMachinePropertiesEntity.getProperty("PropertyValue");
+    System.debug("Found property "+propertyName + " =  " + propertyValue);
+    vmProperties.put(propertyName, propertyValue);
+}
+
+//Workflow Inputs
+var vmName = machine.get("name");
+
+// Logs
+System.log("\n" + "Start Payload WF Input Logs");
+System.log("blueprintName is " + blueprintName);
+System.log("vmName is currently " + vmName);
+System.log("componentId is " + componentId);
+System.log("\n");
